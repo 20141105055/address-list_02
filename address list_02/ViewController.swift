002228be980009c70c7a9d827e2d3cf2ab2cbc57
卢佳明 <571836353@qq.com>
2016-07-07 10:44:07 +0800

    第四版本：通讯录较完整版本
    较上一版本增加了许多功能！！增加了按姓名查找的删除的功能，和按电话查找的删除的功能，还能通过主管理界面可以查询所有用户的录入信息，进而通过信息的
    全部查看功能查看功能是否实现，该版本还没有实现按姓名等条件查找的功能，正在努力实现中！

--- address list_02/ViewController.swift
+++ address list_02/ViewController.swift
@@ -19,25 +19,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         db = SQLiteDB.sharedInstance()
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
-        initUser()
+        db.execute("create table if not exists //user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        //initUser()
     }
     
     @IBAction func saveClicked(sender: AnyObject) {
         saveUser()
     }
     func initUser(){
-        let data = db.query("select * from t_user")
+        let data = db.query("select * from user")
         if data.count > 0{
         let user = data[data.count - 1]
-            txtMobile.text = user["uname"] as? String
-            txtUname.text = user["moble"] as? String
+            txtMobile.text = user["mobile"] as? String
+            txtUname.text = user["uname"] as? String
         }
     }
     func saveUser() {
         let uname = self.txtUname.text!
         let mobile = self.txtMobile.text!
-        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        let sql = "insert into user(uname,mobile) values('\(uname)','\(mobile)')"
         let result = db.execute(sql)
         print(result)
     }
